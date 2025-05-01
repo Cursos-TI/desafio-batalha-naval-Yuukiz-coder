b401a2da7e8132eaa73f38abc09df101b90180a9
Yuukiz-coder <arthurzekk@gmail.com>
2025-04-30 21:08:44 -0300

    Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,64 +1,58 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+//Nao consegui adicionar verificação de sobreposição pois em nenhum momento das aulas foi mencionado isso.
+
+//Constantes declaradas
+#define LINHAS 10
+#define COLUNAS 10
+#define TAMANHONAVIO 3
 
 int main() {
 
     //Tabuleiro
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+    int tabuleiro[LINHAS][COLUNAS];
 
-    //Navios 
-    //Navio1 Horizontal, Navio2 Vertical
-    int navio1[3] = {3, 3, 3}, navio2[3] = {3, 3, 3};
-
-    //Coordenadas dos navios
-    //Vou inserir o navio1 na linha 3 na horizontal a partir da coluna 2
-    int linha3 = 3;
-    int coluna = 2;
-
-    //Vou inserir o navio2 na coluna 7 na vertical a partir da linha 0
-    int coluna7 = 7;
-    int linha = 0;
-
-    //for para inserir navio1
-    for(int i = 0; i < 3; i++) {
-        tabuleiro[linha3][coluna + i] = navio1[i];
+    //Tabuleiro com 0
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            tabuleiro[i][j] = 0;   
+        }
     }
 
-    //for para inserir o navio2
-    for(int i = 0; i < 3; i++) {
-        tabuleiro[linha + i][coluna7] = navio2[i];
+    //Coordenadas dos navios
+
+    //Vou inserir o navio1 na linha 3 na horizontal a partir da coluna 2
+    for (int i = 0; i < TAMANHONAVIO; i++) {
+        tabuleiro[3][2 + i] = 3;
+    }
+    //Vou inserir o navio2 na coluna 7 na vertical a partir da linha 0
+    for (int i = 0; i < TAMANHONAVIO; i++) {
+        tabuleiro[0 + i][7] = 3;
+    }
+
+    //Vou inserir o navio3 na coluna 5 linha 5 até a coluna 8 linha 8
+    for (int i = 0; i < TAMANHONAVIO; i++) {
+        tabuleiro[5 + i][5 + i] = 3;
+    }
+    
+    //Vou inseir o navio4 na coluna 2 linha 0 até a coluna 0 linha 2
+    for (int i = 0; i < TAMANHONAVIO; i++) {
+        tabuleiro[0 + i][2 - i] = 3;
     }
 
     //Mensagem inicial
     printf("\n---Tabuleiro Batalha Naval em C---\n\n");
 
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++) {
+    //Exibição do tabuleiro com os barcos posicionados
+    for(int i = 0; i < LINHAS; i++) {
+        for(int j = 0; j < COLUNAS; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
 
+    //Mensagem final
     printf("\nFim!\n");
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
